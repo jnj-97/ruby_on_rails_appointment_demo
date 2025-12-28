572e177f780de0ccbc3413741e8716f918ba71d1
jnj-97 <nobinjohnson3@gmail.com>
2025-12-28 18:21:39 +0530

    Initial commit

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,54 @@
+class RoomsController < ApplicationController
+  before_action :set_room, only: %i[show edit update destroy]
+
+  def index
+    @rooms = Room.all
+  end
+
+  def show
+  end
+
+  def new
+    @room = Room.new
+  end
+
+  def edit
+  end
+
+  def create
+    @room = Room.new(room_params)
+
+    respond_to do |format|
+      if @room.save
+        format.html { redirect_to room_url(@room), notice: "Room was created successfully" }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @room.update(room_params)
+        format.html { redirect_to room_url(@room), notice: "Room was updated" }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @room.destroy
+    redirect_to rooms_url, notice: "Room was deleted successfully"
+  end
+
+  private
+
+  def set_room
+    @room = Room.find(params[:id])
+  end
+
+  def room_params
+    params.require(:room).permit(:name, :capacity)
+  end
+end
